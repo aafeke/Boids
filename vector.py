84d0400ffa1bd7c198bdde5eba01c0f1337be60b
apri44 <44afk44@gmail.com>
2022-03-07 18:03:05 +0300

    Revert "all vector intances can now do time management to calculate delta"
    This reverts commit 961f62a0aaacefb47385595f5a30b3da051ef53a.

--- vector.py
+++ vector.py
@@ -1,36 +1,19 @@
 import time
 
+initiated = False
+start = 0
+
 
 class vector:
     def __init__(self, greatness=0, angle=0):
         self.vector[0] = greatness
         self.vector[1] = angle
-        self.set_greatness(0)
 
-    def set_greatness(self, val: float):
-        if not val == 0:
-            self.vector[0] = val
 
-            # We should get the time in order to
-            # calculate the delta time while greatness of the
-            # vector != 0
-            self.__set_time()
-        else:
-            self.vector[0] = 0
-            self.__reset_time()
+    @classmethod
+    def set_time(cls):
+        vector.start = time.time()
 
-    def get_delta(self):
-        if self.start_time is not 0:
-            return time.time() - self.start_time
-        else: pass
-
-    def __set_time(self):
-        # Double under score ensures the method is
-        # private, used for the sake of information hiding.
- 
-        self.start_time = time.time()
-
-    def __reset_time(self):
-
-        self.start_time = 0
-        pass
+    @classmethod
+    def get_time(cls):
+        return time.time() - vector.start
